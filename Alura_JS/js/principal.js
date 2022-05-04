cb383b844e5c00d9a660114b4e27a740eb59e8e0
Rafael Lima da Silva <78000302+RafaFX@users.noreply.github.com>
2022-05-04 14:02:34 -0300

    second_commit
    JavaScript: programando na linguagem da web
    02. Variáveis e Operadores

--- Alura_JS/js/principal.js
+++ Alura_JS/js/principal.js
@@ -1,3 +1,43 @@
 var titulo = document.querySelector(".titulo");
+titulo.textContent = "Health Life";
 
-titulo.textContent = "Ovo"
+
+
+//variaveis para calculo imc
+var paciente =  document.querySelector("#primeiro_paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+//variavel para imc aparecer automaticamente na page web
+
+var tdImc = paciente.querySelector(".info-imc");
+
+//validações
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000 ){
+    console.log("Peso inválido");
+    var pesoEhValido = false;
+    tdImc.textContent = "Peso inválido";
+}
+
+if (altura < 0 || altura > 3.00 ){
+    console.log("Altura inválida");
+    var alturaEhValida = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+if ( pesoEhValido && alturaEhValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+
+
+
