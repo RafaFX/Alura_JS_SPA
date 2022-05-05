7f7cbb70b4e3ddbcf7a62a9c667ca4312107d755
Rafael Lima da Silva <78000302+RafaFX@users.noreply.github.com>
2022-05-05 12:31:42 -0300

    third_commit
    JavaScript: programando na linguagem da web
    03. Arrays, Loop e Estilos

--- Alura_JS/js/principal.js
+++ Alura_JS/js/principal.js
@@ -1,42 +1,58 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Health Life";
 
+//querySelector = seleciona apenas um elemento como regra o primeiro que aparecer. querySelectorAll = seleciona todos os elementos da pagina
+
+    //for para loop
 
 
-//variaveis para calculo imc
-var paciente =  document.querySelector("#primeiro_paciente");
+    var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    for (var i = 0; i < pacientes.length; i++) {
 
-//variavel para imc aparecer automaticamente na page web
+        var paciente = pacientes[i]; //mudando a variavel durante o for 
 
-var tdImc = paciente.querySelector(".info-imc");
+        //variaveis para o calculo imc
 
-//validações
+        var tdPeso = paciente.querySelector(".info-peso");
+        var peso = tdPeso.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+        var tdAltura = paciente.querySelector(".info-altura");
+        var altura = tdAltura.textContent;
 
-if (peso <= 0 || peso >= 1000 ){
-    console.log("Peso inválido");
-    var pesoEhValido = false;
-    tdImc.textContent = "Peso inválido";
-}
+        var tdImc = paciente.querySelector(".info-imc");
 
-if (altura < 0 || altura > 3.00 ){
-    console.log("Altura inválida");
-    var alturaEhValida = false;
-    tdImc.textContent = "Altura inválida";
-}
+        var pesoEhValido = true;
+        var alturaEhValida = true;
 
-if ( pesoEhValido && alturaEhValida){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
+        //validações
+
+        if (peso <= 0 || peso >= 1000) {
+            console.log("Peso inválido!");
+            pesoEhValido = false;
+            tdImc.textContent = "Peso inválido";
+            paciente.classList.add("paciente-invalido")                        //A propriedade "classList" retorna todas as classes do objeto e nos permite ter acesso a metodos como o "add" que adiciona uma classe ao objeto
+        }
+
+        if (altura <= 0 || altura >= 3.00) {
+            console.log("Altura inválida!");
+            alturaEhValida = false;
+            tdImc.textContent = "Altura inválida";
+            paciente.classList.add("paciente-invalido")
+        }
+
+        //calculo imc + mostar na pagina web
+
+    if (alturaEhValida && pesoEhValido) {
+            var imc = peso / (altura * altura);
+            tdImc.textContent = imc.toFixed(2);// toFixed função para escolher quantas casas decimais mostrar
+        }
+    }
+
+
+
+
 
 
 
